4052b3136db8b195b8ab563766a43b49cd06deae
Prakash Vishwakarma <prakash2033@outlook.com>
2023-04-01 21:09:09 +0200

    clean up

--- normal_user/nvim/.config/nvim/lua/prakash2033/remap.lua
+++ normal_user/nvim/.config/nvim/lua/prakash2033/remap.lua
@@ -10,17 +10,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- next greatest remap ever
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
