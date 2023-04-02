c9fe6f3ab2e7c373f24e3a8a1fea2cff170b8f69
Prakash Vishwakarma <prakash2033@outlook.com>
2023-04-02 22:47:06 +0200

    tmux open new session with prefix & S

--- normal_user/nvim/.config/nvim/lua/prakash2033/core/keymaps.lua
+++ normal_user/nvim/.config/nvim/lua/prakash2033/core/keymaps.lua
@@ -78,7 +78,7 @@ keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available 
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
--- keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+keymap.set("n", "<leader>ggs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 keymap.set("n", "<leader>gs", "<cmd>Git<CR>")
 
 -- harpoon by theprimagen
