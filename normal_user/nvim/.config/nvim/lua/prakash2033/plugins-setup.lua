88ddaa27222f57c650b5d617bbdbae335ba9307c
Prakash Vishwakarma <prakash2033@outlook.com>
2024-03-21 09:53:53 +0100

    deleted plugin indent blankline

--- normal_user/nvim/.config/nvim/lua/prakash2033/plugins-setup.lua
+++ normal_user/nvim/.config/nvim/lua/prakash2033/plugins-setup.lua
@@ -90,7 +90,7 @@ return packer.startup(function(use)
   -- formatting & linting
   use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
   use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
-  use("lukas-reineke/indent-blankline.nvim") --vertical indent line on left
+  -- use("lukas-reineke/indent-blankline.nvim") --vertical indent line on left
 
   -- treesitter configuration
   use({
