d0f9659f7f7991167a4de4edf725ee2d131f9a93
Prakash Vishwakarma <prakash2033@outlook.com>
2026-01-17 13:23:48 +0100

    Adapt to omarchy & nickjj waybar and my customizations on top

--- normal_user/waybar/.config/waybar/scripts/get_updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Check for available Ubuntu updates and print the count
-
-updates=$(apt list --upgradeable 2>/dev/null | grep -v "Listing..." | wc -l)
-if [ "$updates" -gt 0 ]; then
-  echo "$updates"
-else
-  echo "0"
-fi
