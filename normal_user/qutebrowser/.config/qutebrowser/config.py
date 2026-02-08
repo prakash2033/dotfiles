3326f2c66387a14d886e9055eae2351cf2a6fe90
Prakash Vishwakarma <prakash2033@outlook.com>
2026-02-08 13:19:14 +0100

    qutebrowser keybind Z for yt-dlp

--- normal_user/qutebrowser/.config/qutebrowser/config.py
+++ normal_user/qutebrowser/.config/qutebrowser/config.py
@@ -36,7 +36,7 @@ c.downloads.location.prompt = False
 
 # Bindings for normal mode
 config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
+config.bind('Z', 'hint links spawn ghostty -e youtube-dl {hint-url}')
 config.bind('t', 'set-cmd-text -s :open -t')
 config.bind('xb', 'config-cycle statusbar.show always never')
 config.bind('xt', 'config-cycle tabs.show always never')
